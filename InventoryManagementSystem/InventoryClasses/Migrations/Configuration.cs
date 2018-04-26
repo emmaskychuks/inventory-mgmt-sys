51adae6fd2ec9f0d66c8bd1cd834137a82a9068b
Andrew Knieriem <M0058282@itc.global.mahle>
2018-04-26 00:36:08 -0400

    Started database seed function

--- InventoryManagementSystem/InventoryClasses/Migrations/Configuration.cs
+++ InventoryManagementSystem/InventoryClasses/Migrations/Configuration.cs
@@ -18,6 +18,100 @@ namespace InventoryClasses.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            context.Customers.AddOrUpdate(
+                new Customer()
+                {
+                    CustomerID = 1,
+                    Address = "9876 Boogie Street",
+                    Name = "Iron Man", 
+                    Phone = "945-345-6294",
+                },
+                new Customer()
+                {
+                    CustomerID = 2,
+                    Address = "392 Monster Ave",
+                    Name = "Incredible Hulk",
+                    Phone = "362-739-7894",
+                },
+                new Customer()
+                {
+                    CustomerID = 3,
+                    Address = "2456 Dorito Blvd.",
+                    Name = "Loki Odinson",
+                    Phone = "362-739-7894",
+                },
+                new Customer()
+                {
+                    CustomerID = 4,
+                    Address = "737 Woot Street",
+                    Name = "The Vision",
+                    Phone = "739-4534-4749",
+                },
+                new Customer()
+                {
+                    CustomerID = 5,
+                    Address = "5253 Tee Street",
+                    Name = "Spiderman",
+                    Phone = "353-689-2647",
+                },
+                new Customer()
+                {
+                    CustomerID = 6,
+                    Address = "53467 Wakanda",
+                    Name = "Black Panther",
+                    Phone = "676-343-0808",
+                }
+            );
+
+
+            context.ItemCategories.AddOrUpdate(
+                new ItemCategory()
+                {
+                    ItemCategoryID = 1,
+                    Name = "Paperclips",
+                    Description = "Little metal clippy things that holds things together",
+                    LocationInWarehouse = "Secion A",
+                    MaxPerWarehouse = 10000,
+                    Price = 0.01M,
+                    RestockItemLimit = 1000,
+                },  
+                new ItemCategory()
+                {
+                    ItemCategoryID = 2,
+                    Name = "Stapler",
+                    Description = "The mother of staples",
+                    LocationInWarehouse = "Secion B",
+                    MaxPerWarehouse = 500,
+                    Price = 8.70M,
+                    RestockItemLimit = 250,
+                },
+                new ItemCategory()
+                {
+                    ItemCategoryID = 3,
+                    Name = "Staples",
+                    Description = "There are many",
+                    LocationInWarehouse = "Secion C",
+                    MaxPerWarehouse = 10000,
+                    Price = 8.70M,
+                    RestockItemLimit = 250,
+                }
+
+            );
+
+
+            context.Vendors.AddOrUpdate(
+                new Vendor()
+                {
+                    VendorID = 1,
+                    Name = "Vendor 1",
+                    ItemProvided = 
+                }
+
+            );
+
+
+
         }
     }
 }
